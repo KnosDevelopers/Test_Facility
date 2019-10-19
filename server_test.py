43712a65e59c614aa26dde0c4280917c9a0092d4
Muhammad Khizar Khan <33714845+KhizarCodes@users.noreply.github.com>
2019-10-20 03:49:24 +0500

    successfull run. simple listening server script

---
+++ server_test.py
@@ -0,0 +1,22 @@
+# Testing a simple implementation of listening server.
+
+import socket
+
+host = '192.168.18.68' #local IPV4 address of the machine
+port = 2030 #any port above the range of 1020
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as server:
+    server.bind((host,port))
+    server.listen()
+    connection,address = server.accept()
+    
+    print("Server estabilished. Waiting for connections...")
+
+    with connection:
+        print("New connection from " + str(address))
+        
+        data = connection.recv(1024)
+        if data == b"Master":
+            connection.send(b"Permission Granted")
+        else:
+            print(data)
