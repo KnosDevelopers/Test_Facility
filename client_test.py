c40d288b2e360d1af59944dc205771231267c692
Muhammad Khizar Khan <33714845+KhizarCodes@users.noreply.github.com>
2019-10-22 11:48:25 +0500

    Modified the client to send and wait for the echo and also added exception handling

--- client_test.py
+++ client_test.py
@@ -6,27 +6,37 @@ host = "192.168.18.89" #the local IP of the server
 port = 2030 #the same port of the server
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as server:
-    
-    print("Trying to estabilish connection with the server...")
-   
-    server.connect((host,port)) #trying to connect
+    try:
+        print("Initiating connection to the server at ",host)
+        server.connect((host,port))
+        print("\nEstabilished connection")
+    except:
+        print("\nCan\'t connect due to some error")
+    finally:
+        continue
 
+    print("\nSend data to the server to listen its echo.")
 
-    """if data == b"Permission Granted":
-        print("Connection estabilished successfully to server")
-    else:
-        print(data)"""
+    while True:
+        usr_data = input("\nEnter data: ")
+        echo_heard = False
 
-    a = "continue"
-    while a!='exit':
-        a=input('Enter a message: ')
-        
-        if a=="exit":
-            break
-        else:
-            server.send(bytes(a.encode()))
-            x = server.recv(1024)
-            print(x.decode())
-        
+        while echo_heard == False:
+            try:
+                print("\nSending data to ",host)
+                server.send(byte(usr_data.encode()))
+            except:
+                print("\nThere was a problem in sending data to the server")
+            finally:
+                continue
 
-        
+            try:
+                data_recv = server.recv(1024)
+                print("\nEcho: ",data_recv.decode())
+                echo_heard = True
+            except:
+                print("\nThere was a problem in receiving data from server")
+                echo_heard = False
+            finally:
+                continue
+
