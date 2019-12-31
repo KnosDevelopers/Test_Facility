17e51acfad269acc46cbb0b7265acf86729ba2ef
khizarcodes <khizer.khan98@gmail.com>
2020-01-01 02:03:30 +0500

    Bug fix. Added headless parameter along with others

--- backend/backend_script.py
+++ backend/backend_script.py
@@ -35,6 +35,9 @@ def main():
 
 def port_updater():
     options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     options.binary_location = '/usr/bin/chromium-browser'
 
     browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',chrome_options=options)
