13f62660457eb3eaa9b19605877b65a94272690d
khizarcodes <khizer.khan98@gmail.com>
2019-12-31 20:23:12 +0500

    Auto port GET function implemented.

--- backend/new_client_script.py
+++ backend/new_client_script.py
@@ -1,29 +1,45 @@
 #Testing a simple implementation of Client program
 
 import socket
+import smtplib
+import pymongo
 
-host = "0.tcp.ngrok.io" #the local IP of the server
-port = 16617 #the same port of the server
+def main(port_no):
+    host = "0.tcp.ngrok.io" #the local IP of the server
+    port = port_no #the same port of the server
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-try:
-    print("\nInitiating connection to the server")
-    server.connect((host,port)) 
-    print("Connected to the server at",host)
-except:
-    print("\nCan\'t connect due to some error")
-    quit()
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        print("\nInitiating connection to the server")
+        server.connect((host,port)) 
+        print("Connected to the server at",host)
+    except:
+        print("\nCan\'t connect due to some error")
+        quit()
 
-print("\nSend data to the server to listen its echo.")
+    print("\nSend data to the server to listen its echo.")
 
-while True:
-    data = str(input("\nType something: "))
-    server.send(data.encode('ascii'))
-    data_recv = server.recv(1024)
-    print("Received from server : ",str(data_recv.decode('ascii')))
-    ans = input("\nDo you want to continue? (y/n)")
-    if ans=='y':
-        continue
-    else:
-        break
-server.close()
+    while True:
+        data = str(input("\nType something: "))
+        server.send(data.encode('ascii'))
+        data_recv = server.recv(1024)
+        print("Received from server : ",str(data_recv.decode('ascii')))
+        ans = input("\nDo you want to continue? (y/n)")
+        if ans=='y':
+            continue
+        else:
+            break
+    server.close()
+
+def port_checker():
+    
+    client = pymongo.MongoClient("mongodb+srv://portmaster:freehitportmaster@testacoda-001-cj5sj.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.freehit_db
+    collection = db.port
+    cursor = collection.find({})
+    for document in cursor:
+        port_no = document['port']
+    
+    return port_no
+    
+main(port_checker())
